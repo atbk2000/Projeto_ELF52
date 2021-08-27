9d77c55c24dc3cfb028f0a49770eca7e91eeee13
atbk2000 <73497758+atbk2000@users.noreply.github.com>
2021-08-27 11:24:48 -0300

    Add files via upload

---
+++ src/init.s
@@ -0,0 +1,125 @@
+        PUBLIC GPIO_enable
+        PUBLIC GPIO_special
+        PUBLIC GPIO_select
+        PUBLIC UART_enable
+        PUBLIC UART_config
+        
+        SECTION .text:CODE:REORDER(1)
+        
+        THUMB 
+        
+; System Control definitions
+SYSCTL_BASE             EQU     0x400FE000
+SYSCTL_RCGCGPIO         EQU     0x0608
+SYSCTL_PRGPIO		EQU     0x0A08
+SYSCTL_RCGCUART         EQU     0x0618
+SYSCTL_PRUART           EQU     0x0A18
+
+; GPIO port definitions
+GPIO_AFSEL              EQU     0x0420
+GPIO_DEN                EQU     0x051C
+GPIO_PCTL               EQU     0x052C
+
+; UART definitions
+UART_PORT0_BASE         EQU     0x4000C000
+UART_FR                 EQU     0x0018
+UART_IBRD               EQU     0x0024
+UART_FBRD               EQU     0x0028
+UART_LCRH               EQU     0x002C
+UART_CTL                EQU     0x0030
+UART_CC                 EQU     0x0FC8
+
+
+
+; GPIO_enable: habilita clock para os ports de GPIO selecionados em R2
+; R2 = padrão de bits de habilitação dos ports
+; Destrói: R0 e R1
+GPIO_enable:
+        LDR R0, =SYSCTL_BASE
+	LDR R1, [R0, #SYSCTL_RCGCGPIO]
+	ORR R1, R2 ; habilita ports selecionados
+	STR R1, [R0, #SYSCTL_RCGCGPIO]
+
+waitg	LDR R1, [R0, #SYSCTL_PRGPIO]
+	TEQ R1, R2 ; clock dos ports habilitados?
+	BNE waitg
+
+        BX LR
+
+
+; GPIO_special: habilita funcões especiais no port de GPIO desejado
+; R0 = endereço base do port desejado
+; R1 = padrão de bits (1) a serem habilitados como funções especiais
+; Destrói: R2
+GPIO_special:
+	LDR R2, [R0, #GPIO_AFSEL]
+	ORR R2, R1 ; habilita bits 1 e 0 do registrador GPIOAFSEL
+	STR R2, [R0, #GPIO_AFSEL]
+
+	LDR R2, [R0, #GPIO_DEN]
+	ORR R2, R1 ; habilita função digital
+	STR R2, [R0, #GPIO_DEN]
+
+        BX LR
+        
+        
+; GPIO_select: seleciona funcões especiais no port de GPIO desejado
+; R0 = endereço base do port desejado
+; R1 = máscara de bits a serem alterados
+; R2 = padrão de bits (1) a serem selecionados como funções especiais
+; Destrói: R3
+GPIO_select:
+	LDR R3, [R0, #GPIO_PCTL]
+        BIC R3, R1 ; limpa apenas as funções especiais no port A (bits 1 e 0)
+	ORR R3, R2 ; seleciona funções especiais
+	STR R3, [R0, #GPIO_PCTL]
+
+        BX LR
+
+
+; UART_enable: habilita clock para as UARTs selecionadas em R2
+; R2 = padrão de bits de habilitação das UARTs
+; Destrói: R0 e R1
+UART_enable:
+        LDR R0, =SYSCTL_BASE
+	LDR R1, [R0, #SYSCTL_RCGCUART]
+	ORR R1, R2 ; habilita UARTs selecionados
+	STR R1, [R0, #SYSCTL_RCGCUART]
+
+waitu	LDR R1, [R0, #SYSCTL_PRUART]
+	TEQ R1, R2 ; clock das UARTs habilitados?
+	BNE waitu
+
+        BX LR
+        
+ 
+; UART_config: configura a UART desejada
+; R0 = endereço base da UART desejada
+; Destrói: R1
+UART_config:
+        LDR R0, =UART_PORT0_BASE
+        LDR R1, [R0, #UART_CTL]
+        BIC R1, #0x01 ; desabilita UART (bit UARTEN = 0)
+        STR R1, [R0, #UART_CTL]
+
+        ; clock = 16MHz, baud rate = 9600 bps
+        MOV R1, #104
+        STR R1, [R0, #UART_IBRD]
+        MOV R1, #11
+        STR R1, [R0, #UART_FBRD]
+        
+        ; 8 bits, 1 stop, odd parity, FIFOs disabled, no interrupts, 
+        MOV R1, #01100010b
+        STR R1, [R0, #UART_LCRH]
+        
+        ; clock source = system clock
+        MOV R1, #0x00
+        STR R1, [R0, #UART_CC]
+        
+        LDR R1, [R0, #UART_CTL]
+        ORR R1, #0x01 ; habilita UART (bit UARTEN = 1)
+        STR R1, [R0, #UART_CTL]
+
+        BX LR
+        
+        END
